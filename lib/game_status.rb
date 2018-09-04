8e91d46eb6f9de3755d53ac0c1dcb76a14cc19f0
Ted Barlow <ted@noukod.com>
2018-09-04 19:02:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,15 +31,15 @@ def won?(board)
 end
 
 def full?(board)
-  #if board.all?{|cell| cell == "X" || cell == "O"}
+  if board.all?{|cell| board[cell] == "X" || board[cell] == "O"}
+  end
+  #x_array = board.select do |cell|
+    #cell == "X"
   #end
-  x_array = board.select do |cell|
-    cell == "X"
-  end
-  o_array = board.select do |cell|
-    cell == "O"
-  end
-  ((x_array + o_array) == 9)
+  #o_array = board.select do |cell|
+    #cell == "O"
+  #end
+  #((x_array + o_array) == 9)
 end
 
 def draw?(board)
