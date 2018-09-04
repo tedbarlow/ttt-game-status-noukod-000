0e4ef7762ffd45b26b6df4b228d54f7c61b97743
Ted Barlow <ted@noukod.com>
2018-09-04 19:30:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -43,8 +43,8 @@ def over?(board)
 end
 
 def winner?(board)
+  game_winner = nil
   if won?(board)
-    game_winner = nil
     WIN_COMBINATIONS.each do |combinations|
       if combinations.all?{|cell| board[cell] == "X"}
         game_winner = "X"
